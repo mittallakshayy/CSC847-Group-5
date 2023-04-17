ba077abb2afc1a5642750f0fec12e94b183ea7b5
Ekarat Buddharuksa <ebuddharuksa@sfsu.edu>
2023-04-16 18:59:34 -0700

    comment out env for deploying on GCP

--- backend/credentials.py
+++ backend/credentials.py
@@ -4,5 +4,5 @@ import configparser
 key_praser = configparser.ConfigParser()
 key_praser.read("credentials.ini")
 
-filename = key_praser.get("credentials", "key_path")
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.abspath(f'../{filename}')
+#filename = key_praser.get("credentials", "key_path")
+#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.abspath(f'../{filename}')
